53c382e0b76f937e743c076e48d67212d71622f5
Unknown <julia.shreckhise@gmail.com>
2018-05-14 20:49:11 -0400

    working on model associations

--- models/user.js
+++ models/user.js
@@ -2,10 +2,17 @@
 // Creating the model for the user 
 // information exporting and returning as user
 module.exports = function(sequelize, DataTypes){
-    var User = sequelize.define("User", {
+    var User = sequelize.define("Users", {
         name: DataTypes.STRING,
         password: DataTypes.STRING,
-        email: DataType.STRING
+        email: DataTypes.STRING
     });
+
+    User.associate = function(models) {
+        User.hasMany(models.BlogPost, {
+          onDelete: "cascade"
+        });
+      };
+
     return User;
-}
+};
