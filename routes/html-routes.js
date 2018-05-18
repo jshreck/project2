2ea80c66c708aa556860fc76d62c077ac926460b
Unknown <julia.shreckhise@gmail.com>
2018-05-18 13:22:38 -0400

    handlebars working, get routes working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,10 +1,21 @@
 var path = require("path");
 
-module.exports= function(app){
-    app.get("/", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    })
-     
-}
+module.exports = function (app) {
+    //for login page
+    app.get("/", (req, res) => {
+        res.render("index");
+    });
 
-
+    //for handlebars templates... id is name of the file, ex: template0
+    app.get("/templates/:id", (req, res) => {
+        template = req.params.id;
+        //just using for example
+        var hbsObj = {
+            blogPost: [{
+                title: "Example",
+                body: "blahblahblah"
+            }]
+        }
+        res.render(template, hbsObj);
+    });
+};
--- server.js
+++ server.js
@@ -17,12 +17,29 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Set Handlebars up
+var exphbs = require("express-handlebars");
+
+//was here before
+app.engine("handlebars", exphbs({
+  defaultLayout: "main",
+  helpers: {
+    section: function(name, options) { 
+      if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this); 
+        return null;
+      }
+  }
+}));
+app.set("view engine", "handlebars");
+
 //to be able to use different methods than html allows
 app.use(methodOverride('_method'));
 
+
 // Import routes and give the server access to them
-var routes = require("./routes/html-routes.js");
-app.use(routes);
+require("./routes/html-routes.js")(app);
+
 
 //added the db require for models and sync with promise
 db.sequelize.sync().then(function(){
