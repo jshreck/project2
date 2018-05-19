d392f2cc07a78f7010f1dff53f6069d16b0181a5
Unknown <alvaro@alvarobeckett.com>
2018-05-19 12:41:30 -0400

    Auto stash before merge of "alvaro" and "origin/develop"

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ var exphbs = require("express-handlebars");
 
 //was here before
 app.engine("handlebars", exphbs({
-  defaultLayout: "main",
-  helpers: {
-    section: function(name, options) { 
-      if (!this._sections) this._sections = {};
-        this._sections[name] = options.fn(this); 
-        return null;
-      }
-  }
+    defaultLayout: "main",
+    helpers: {
+        section: function(name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
 }));
 app.set("view engine", "handlebars");
 
@@ -42,8 +42,8 @@ require("./routes/html-routes.js")(app);
 require("./routes/user-api-routes.js")(app);
 
 //added the db require for models and sync with promise
-db.sequelize.sync().then(function(){
-app.listen(PORT, function () {
-  console.log("App now listening at localhost:" + PORT);
-});
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log("App now listening at localhost:" + PORT);
+    });
 });
